29cdb96c0d3260eabf40c405c7bb36daabc27228
Igor Guselnikov <guselnikov.uniikm@yandex.ru>
2023-03-25 15:46:30 +0500

    task2

--- src/lesson3/task2.java
+++ src/lesson3/task2.java
@@ -1,2 +1,12 @@
-package lesson3;public class task2 {
+package lesson3;
+
+public class task2 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 8;
+
+        int result = 0;
+        System.out.println(result = a-- - --a + ++a + a++ + a);
+        System.out.println(result = ++b - b++ + ++b - --b);
+    }
 }
