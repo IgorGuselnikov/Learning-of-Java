8fe916cd17895ab63125d9ae5b15470f9d367ec7
Igor Guselnikov <guselnikov.uniikm@yandex.ru>
2023-03-26 10:47:50 +0500

    task about info of students edit

--- src/lesson4/Student.java
+++ src/lesson4/Student.java
@@ -7,9 +7,14 @@ public class Student {
     String firstname;
     String lastname;
     int years_of_study;
-    double avarenge_value_math;
-    double avarenge_value_economic;
+    double averenge_value_math;
+    double averenge_value_economic;
     double averenge_value_forign_language;
+
+    void averenge_grade(double a, double b, double c){
+        double result = (a + b + c)/3;
+        System.out.println(result);
+    }
 }
 class StudentTest{
     public static void main(String[] args) {
@@ -21,31 +26,31 @@ class StudentTest{
         person1.firstname = "Igor";
         person1.lastname = "Guselnikov";
         person1.years_of_study = 2005;
-        person1.avarenge_value_math = 4.6;
-        person1.avarenge_value_economic = 4.72;
+        person1.averenge_value_math = 4.6;
+        person1.averenge_value_economic = 4.72;
         person1.averenge_value_forign_language = 4.16;
 
         person2.number_id = 002;
         person2.firstname = "Egor";
         person2.lastname = "Shelpakov";
         person2.years_of_study = 2005;
-        person2.avarenge_value_math = 4.11;
-        person2.avarenge_value_economic = 3.89;
+        person2.averenge_value_math = 4.11;
+        person2.averenge_value_economic = 3.89;
         person2.averenge_value_forign_language = 4.97;
 
         person3.number_id = 002;
         person3.firstname = "Tatyana";
         person3.lastname = "Chertanova";
         person3.years_of_study = 2001;
-        person3.avarenge_value_math = 4.08;
-        person3.avarenge_value_economic = 4.92;
+        person3.averenge_value_math = 4.08;
+        person3.averenge_value_economic = 4.92;
         person3.averenge_value_forign_language = 4.99;
 
-        System.out.println("Средняя арифметическая оценка " + person1.firstname + " " + person1.lastname + " имеет значение равное = "
-                + ((person1.avarenge_value_math + person1.avarenge_value_economic + person1.averenge_value_forign_language)/3));
-        System.out.println("Средняя арифметическая оценка " + person2.firstname + " " + person2.lastname + " имеет значение равное = "
-                + (person2.avarenge_value_math + person2.avarenge_value_economic + person2.averenge_value_forign_language)/3);
-        System.out.println("Средняя арифметическая оценка " + person3.firstname + " " + person3.lastname + " имеет значение равное = "
-                + (person3.avarenge_value_math + person3.avarenge_value_economic + person3.averenge_value_forign_language)/3);
+        System.out.print("Среднюю арифметическую оценку " + person1.firstname + " " + person1.lastname + " имеет значение равное = ");
+        person1.averenge_grade(person1.averenge_value_math, person1.averenge_value_economic, person1.averenge_value_forign_language);
+        System.out.print("Среднюю арифметическую оценку " + person2.firstname + " " + person2.lastname + " имеет значение равное = ");
+        person2.averenge_grade(person2.averenge_value_math, person2.averenge_value_economic, person2.averenge_value_forign_language);
+        System.out.print("Среднюю арифметическую оценку " + person3.firstname + " " + person3.lastname + " имеет значение равное = ");
+        person3.averenge_grade(person3.averenge_value_math, person3.averenge_value_economic, person3.averenge_value_forign_language);
     }
 }
